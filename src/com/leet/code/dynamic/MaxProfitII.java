001f1fff4079b90f6f55ff3dbb2e375b1177f0ac
merdocks <2231335766@qq.com>
2023-04-20 17:15:41 +0800

    MaxProfit

--- src/com/leet/code/dynamic/MaxProfitII.java
+++ src/com/leet/code/dynamic/MaxProfitII.java
@@ -8,7 +8,15 @@ package com.leet.code.dynamic;
 public class MaxProfitII {
 
     public int maxProfit(int[] prices) {
+        //dp[i][0] 表示第i天持有股票所得现金。
+        //dp[i][1] 表示第i天不持有股票所得最多现金
         int[][] dp=new int[prices.length][2];
-return 0;
+        dp[0][0]=-prices[0];
+        dp[0][1]=0;
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0]=Math.max(dp[i-1][0],dp[i-1][1]+prices[i]);
+            dp[i][1]=Math.max(dp[i-1][i],dp[i-1][0]-prices[i]);
+        }
+        return dp[prices.length-1][0];
     }
 }
